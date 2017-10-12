af1a352390094b11858e3b445cde1a10f232bbde
Hasan <hasan.shobri@outlook.com>
2017-10-12 23:12:54 +0700

    first commit

---
+++ app/src/main/java/me/herlambang/recipeapps/Data/RecipeData.java
@@ -0,0 +1,80 @@
+package me.herlambang.recipeapps.Data;
+
+import java.util.ArrayList;
+
+import me.herlambang.recipeapps.Adapter.RecipeAdapter;
+
+public class RecipeData {
+    public static String[][] data = new String[][]{
+            {"Sayur Bayam Bening ", "Makanan Rumah",
+                    "http://resepnasional.com/wp-content/uploads/2014/10/Cara-Membuat-Sayur-Bayam-Bening-300x221.jpg",
+                    "Salah satu resep masakan sehari-hari yang wajib kita sajikan sebagai selingan menu dirumah adalah sayur bayam bening ini. Resep ini merupakan warisan dari orang tua kita dahulu. Cara membuat beserta bahan dan bumbu yang diperlukan untuk membuat sayur bening ini sangat praktis dan mudah didapat.\n" +
+                            "Bahan yang dibutuhkan adalah :\n" +
+                            "– Bayam segar kurang lebih 2 ikat saja. Cuci bersih lalu petik dan buang bagian pangkalnya.\n" +
+                            "– Buah tomat ukuran sedang besar kurang lebih sebanyak 1 pcs.\n" +
+                            "– Bawang putih kurang lebih sebanyak 1 pcs saja. Kupas kulitnya lalu iris iris tipis.\n" +
+                            "– Jagung manis segar yang sudah dipotong potong sesuai selera kurang lebih sebanyak 1 pcs saja.\n" +
+                            "– Gula pasir putih secukupnya sesuai selera.\n" +
+                            "– Kaldu instan rasa ayam secukupnya sesuai selera.\n" +
+                            "– Garam dapur beryodium secukupnya sesuai selera.\n" +
+                            "– Air bersih secukupnya\n" +
+                            "Cara Membuatnya :\n" +
+                            "– Siapkan panci dan beri air secukupnya. Masak sampai mendidih.\n" +
+                            "– Masukkan potongan jagung manisnya bersama dengan potongan jagung manisnya.\n" +
+                            "– Aduk aduk sebentar dan masak sampai jagungnya hampir matang.\n" +
+                            "– Masukkan sayuran bayam bersama irisan buah tomatnya dan kaldu instan ayam secukupnya. Aduk aduk sampai semua bahan tercampur rata.\n" +
+                            "– Masukkan gula pasir dan garam dapur beryodium secukupnya sesuai selera dan aduk aduk kembali sampai semua bahan tercampur rata.\n" +
+                            "– Setelah semua bahan matang, tes rasanya dan tambahkan kaldu, gula dan garam kalau diperlukan.\n" +
+                            "– Angkat dan sajikan resep masakan sehari-hari yang sederhana ini saat masih hangat.\n" +
+                            "\n"
+            },
+            {
+                    "Udang Asam Manis Pedas","Masakan Rumah",
+                    "http://resepcaramemasak.info/wp-content/uploads/2015/05/Resep-Udang-Asam-Manis.jpg",
+                    "1.\tSeperti resep masakan udang lainnya, kali ini kita memerlukan udang windu kualitas bagus sebanyak kurang lebih 500 gram saja. Untuk jenisnya bisa disesuaikan dengan jenis kesukaan masing masing asal ukurannya tidak terlalu besar seperti udang lobster.\n" +
+                            "2.\tBawang merah lokal ukuran sedang besar kurang lebih sebanyak 10 siung saja. Kupas kulit luarnya lalu iris kecil kecil supaya mudah saat nanti akan dihaluskan (bumbu halus).\n" +
+                            "3.\tTomat ukuran sedang besar kurang lebih sebanyak 2 buah saja. Cuci bersih lalu potong menjadi beberapa bagian supaya mudah saat akan dihaluskan (bumbu halus).\n" +
+                            "4.\tCabai merah besar supaya warnanya lebih bagus kurang lebih sebanyak 8-10 buah. Bisa ditambahkan atau dikurangi sesuai selera anda (bumbu halus).\n" +
+                            "5.\tBawang putih ukuran sedang besar kurang lebih sebanyak 5-6 siung saja. Kupas kulitnya untuk nanti dihaluskan bersama dengan bumbu lain (bumbu halus).\n" +
+                            "6.\tCabai rawit kurang lebih sebanyak 6-8 pcs atau bisa ditambahkan sesuai dengan selera masing masing (bumbu halus).\n" +
+                            "7.\tSaus sambal botolan kualitas bagus untuk menambah citarasa pedasnya sesuai selera atau kurang lebih sebanyak 1-2 sendok makan saja.\n" +
+                            "8.\tKecap manis kental kurang lebih sebanyak 2 sendok makan saja.\n" +
+                            "9.\tGula pasir untuk menguatkan cita rasa manisnya kurang lebih sebanyak 1-2 sendok makan saja.\n" +
+                            "10.\tMargarin kualitas bagus secukupnya untuk menumis bumbunya atau kurang lebih sebanyak 2 sendok – 3 sendok makan saja.\n" +
+                            "11.\tGaram dapur beryodium secukupnya atau kurang lebih sebanyak 3 sendok kecil saja.\n" +
+                            "12.\tAir asam jawa untuk memperkuat rasa khas asamnya kurang lebih sebanyak dua sendok makan saja (campurkan 2 sendok teh asam jawa dengan kurang lebih 4 sendok air bersih lalu diaduk aduk).\n"
+            },
+            {
+                    "Cumi Asam Manis Spesial","Sarapan Sehat",
+                    "https://resepdanmasakan.com/wp-content/uploads/2015/10/Resep-Cumi-Asam-Manis-Pedas-Spesial.png",
+                    "1.\tBawang bombay ukuran sedang besar kurang lebih sebanyak 1 pcs saja. Kupas kulit luarnya lalu iris iris memanjang kecil.\n" +
+                            "2.\tBawang merah ukuran sedang kecil kurang lebih sebanyak 10 pcs. Kupas kulit luarnya lalu cincang halus.\n" +
+                            "3.\tBawang putih ukuran sedang besar kurang lebih sebanyak 2 siung saja. Kupas kulit bawangnya lalu cincang halus juga supaya aromanya lebih harum.\n" +
+                            "4.\tJahe ukuran sedang kurang lebih sepanjang 0.5 cm saja. Kupas kasar kulit luarnya lalu geprak atau memarkan.\n" +
+                            "5.\tBuah tomat segar kurang lebih sebanyak 1 pcs saja. Cuci bersih lau potong potong bentuk kotak kecil atau serong (tergantung selera).\n" +
+                            "6.\tBuah cabai merah besar secukupnya sesuai selera. Cuci bersih lalu iris iris kecil.\n" +
+                            "7.\tBuah cabai rawit secukupnya sesuai selera. Cuci bersih lalu iris kecil kecil.\n" +
+                            "8.\tSaus sambal kalau suka (bisa dihilangkan karena sudah memakai cabai atau bisa dikombinasikan keduanya).\n" +
+                            "9.\tDaun bawang segar 1 batang saja. Cuci bersih lalu rajang kecil kecil.\n" +
+                            "10.\tKecap secukupnya sesuai selera.\n"
+            }
+    };
+
+    public static ArrayList<RecipeAdapter> getListData() {
+        RecipeAdapter bg = null;
+        ArrayList<RecipeAdapter> list = new ArrayList<>();
+        for (int i = 0; i < data.length; i++) {
+            bg = new RecipeAdapter();
+            bg.setName(data[i][0]);
+            bg.setDesc(data[i][1]);
+            bg.setPhoto(data[i][2]);
+            bg.setRecipe(data[i][3]);
+
+
+            list.add(bg);
+        }
+
+        return list;
+    }
+
+}
